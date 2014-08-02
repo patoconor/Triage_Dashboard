1a87d6e6f76bd3856295e40c733b17d073dd2235
patoconor <patoconor@gmail.com>
2014-08-01 21:06:58 -0400

    added service,error,server,stackTrace,dateTime

--- src/dashboard/ListItem.java
+++ src/dashboard/ListItem.java
@@ -3,14 +3,88 @@ package dashboard;
 public class ListItem {
 
 	private String fileID;
-	private String expectID;
 	private String dateTime;
 	private boolean isFinished;
 	private String devName;
-	private String analystName;
 	private String serviceLocation;
+	private String errorMessage;
+	private String server;
 	private String environment;
+	private String stackTrace;
+	private String service;
+	private String expectID;
+	private String analystName;
 	
+ 
+ 	public String getExpectID() {
+ 		return expectID;
+ 	}
+ 
+ 
+ 	public void setExpectID(String expectID) {
+ 		this.expectID = expectID;
+ 	}
+ 
+ 
+ 	public String getAnalystName() {
+ 		return analystName;
+ 	}
+ 
+ 
+ 	public void setAnalystName(String analystName) {
+ 		this.analystName = analystName;
+ 	}
+	
+	public String getService() {
+		return service;
+	}
+
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+
+
+	public String getStackTrace() {
+		return stackTrace;
+	}
+
+
+	public void setStackTrace(String stackTrace) {
+		this.stackTrace = stackTrace;
+	}
+
+
+	public String getServer() {
+		return server;
+	}
+
+
+	public void setServer(String server) {
+		this.server = server;
+	}
+
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+
 	public ListItem(String fileID)
 	{
 		this.fileID=fileID;
@@ -18,8 +92,22 @@ public class ListItem {
 		devName="";
 		setServiceLocation("");
 	}
-
 	
+	public ListItem(String fileID, String dateTime, String server, String stack, String error, String service)
+	{
+		this.fileID=fileID;
+		this.dateTime = dateTime;
+		this.server = server;
+		this.stackTrace = stack;
+		this.errorMessage = error;
+		this.service = service;
+	}
+	
+	public ListItem() {
+		// TODO Auto-generated constructor stub
+	}
+
+
 	public String getFileID() {
 		return fileID;
 	}
@@ -61,36 +149,7 @@ public class ListItem {
 	public void setServiceLocation(String serviceLocation) {
 		this.serviceLocation = serviceLocation;
 	}
-
-
-	public String getEnvironment() {
-		return environment;
-	}
-
-
-	public void setEnvironment(String environment) {
-		this.environment = environment;
-	}
-
-
-	public String getExpectID() {
-		return expectID;
-	}
-
-
-	public void setExpectID(String expectID) {
-		this.expectID = expectID;
-	}
-
-
-	public String getAnalystName() {
-		return analystName;
-	}
-
-
-	public void setAnalystName(String analystName) {
-		this.analystName = analystName;
-	}
 	
 	
 }
+
